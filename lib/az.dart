e5c68258bf0ff382b66478f42ff7eea45e72788d
ana.zurabashvili <anaz.zurabashvili@gmail.com>
2023-03-04 18:04:14 +0400

    feat(Add scroll positioner)

--- lib/az.dart
+++ lib/az.dart
@@ -1,5 +1,4 @@
 library noxobserver;
 
 export 'package:az_scrolling/alp_scroll/alphabet.dart';
-export 'package:az_scrolling/alp_scroll/alphabet_scroll.dart';
 export 'package:az_scrolling/alp_scroll/az_scrolling.dart';
